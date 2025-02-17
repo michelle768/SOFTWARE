d9f56334348f65ef2e48672b5f4bebc82d87186b
Michelle <michelle768@gmail.com>
2025-02-17 13:38:45 -0400

    codigos de java

---
+++ java/user.java
@@ -0,0 +1,15 @@
+class user extends Account {
+    public user (String name, String
+    document, String email, string
+    password){ 
+        super(name, document, email
+        password);
+    }
+    void printDateUser(){
+        System.out.println("document 
+        driver: " + document + " Name
+        driver: " + name + " E-mail: "
+        + email + " password:"+
+        password);
+    }
+}
